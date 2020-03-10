2c16f5f6e80a07e7a3759e0d43b1a93eaeb82ce4
mavisjheng <erjhen@gmail.com>
2020-03-10 11:00:04 +0800

    Updates

---
+++ precache-manifest.e60225c237cbf644e44246b782aa35c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d3f3292dd1ebb94cbf19c2dde20931",
+    "url": "/pages/mavisjheng/planning-board/index.html"
+  },
+  {
+    "revision": "06670838e69a724f1c28",
+    "url": "/pages/mavisjheng/planning-board/static/css/2.83512975.chunk.css"
+  },
+  {
+    "revision": "93a83736eceff485561d",
+    "url": "/pages/mavisjheng/planning-board/static/css/main.e8c099f0.chunk.css"
+  },
+  {
+    "revision": "06670838e69a724f1c28",
+    "url": "/pages/mavisjheng/planning-board/static/js/2.b24ef738.chunk.js"
+  },
+  {
+    "revision": "b4a638b455b533c8e657f6a60c2ccd14",
+    "url": "/pages/mavisjheng/planning-board/static/js/2.b24ef738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a83736eceff485561d",
+    "url": "/pages/mavisjheng/planning-board/static/js/main.cf53c181.chunk.js"
+  },
+  {
+    "revision": "a4fae72383faa4f005ee",
+    "url": "/pages/mavisjheng/planning-board/static/js/runtime-main.dbb3e952.js"
+  }
+]);
